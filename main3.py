e800fffc749400549e515ca177831d69c76d77d2
amanrajbhar01 <83865682+amanrajbhar01@users.noreply.github.com>
2021-05-08 16:57:45 +0530

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,16 @@
+age = input("Enter Your Age/n")
+age = int(age)
+
+
+if(age>18):
+    print("you can drive a car")
+
+elif(age==18):
+    print("tum to badi heavy driver nikle")
+
+
+else:
+    print("chal nikal chutiye")
+    
+    
+
